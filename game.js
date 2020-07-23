dc236a39c45588205898bdb44994a5c5544866a0
barhoomi <42248297+barhoomi@users.noreply.github.com>
2020-07-23 13:17:01 +0300

    add queuing and block collision boxes

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 canvas = document.querySelector('.canvas')
 var ctx = canvas.getContext('2d');
 var scale = 30;
+var speed = 140;
 
 // GENERAL FUNCTIONS
 function shuffle(array) {
@@ -25,11 +26,6 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function random(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
-}
-
 var tetromino = [
 {type:"I",grid:[[0,0,0,0],[1,1,1,1],[0,0,0,0],[0,0,0,0]],color:"cyan"},
 {type:"O",grid:[[0,1,1,0],[0,1,1,0],[0,0,0,0]],color:"yellow"},
@@ -39,25 +35,59 @@ var tetromino = [
 {type:"J",grid:[[1,0,0],[1,1,1],[0,0,0]],color:"blue"},
 {type:"L",grid:[[0,0,1],[1,1,1],[0,0,0]],color:"orange"},
 ];
-// var queue = [];
+var queue = [];
 
-// function addQueue(){
-//     shuffle(tetromino);
-//     for(let x = 0;x<7;x++){
-//         queue[x] = tetromino[x];
-//     }
-// }
+function addQueue(){
+    shuffle(tetromino);
+    for(let x = 0;x<tetromino.length;x++){
+        queue.push(tetromino[x]);
+    }
+}
 
 class Block {
     constructor(){
-        this.center = [0,0];
+        this.center = {x:4,y:1};
         this.rotation = 0;
+        if(queue.length<=7){
+            addQueue();
+        }
+        if(isNaN(grid[this.center.y][this.center.x])){
+            resetGame();
+        }
+        this.tetromino = queue.shift();
+        this.type = this.tetromino.type;
+        this.grid = this.tetromino.grid;
+        this.color = this.tetromino.color;
+        this.locked = false;
     }
     moveDown(){
-        grid[block.center[1]][block.center[0]] = "I";
+        drawBlock();
         draw();
-        grid[block.center[1]][block.center[0]] = 0;
-        block.center[1] += 1;
+        if(block.locked == true){
+            block = new Block;
+        }
+        else{
+            for(y = 0;y<block.grid.length;y++){
+                for(x=0;x<block.grid[y].length;x++){
+                    if(block.grid[y][x]==1){
+                        grid[block.center.y+(y-1)][block.center.x+(x-1)]= 0;
+                    }
+                }
+            }
+            block.center.y += 1;
+        }
+    }
+}
+
+function drawBlock(){
+    for(y = 0;y<block.grid.length;y++){
+        for(x=0;x<block.grid[y].length;x++){
+            if(block.grid[y][x]==1){
+                grid[block.center.y+(y-1)][block.center.x+(x-1)]={};
+                grid[block.center.y+(y-1)][block.center.x+(x-1)].type=block.type;  
+                grid[block.center.y+(y-1)][block.center.x+(x-1)].locked=false;
+            }
+        }
     }
 }
 
@@ -85,11 +115,11 @@ var grid = [
 ]
 
 function draw(){
-    for(let y = 0;y < grid.length;y++){
-        for(let x = 0;x < grid[y].length;x++){
+    for(y = 0;y < grid.length;y++){
+        for(x = 0;x < grid[y].length;x++){
             if(isNaN(grid[y][x])){
                 let z = grid[y][x];
-                switch(z){
+                switch(z.type){
                     case "I":
                         ctx.fillStyle = "cyan";
                         ctx.fillRect(x*scale,y*scale,scale,scale);
@@ -128,19 +158,111 @@ function draw(){
             }
         };
     };
-}
+};
+
+
+var handleKeyDown = async function (event){
+    keyValue = event.key;
+    switch(keyValue){
+        case "a":
+        case "ArrowLeft":
+            for(y = 0;y<block.grid.length;y++){
+                for(x=0;x<block.grid[y].length;x++){
+                    if(block.grid[y][x]==1){
+                        // console.log(y,x)
+                        // console.log("type: ",block.type," center: ",block.center.x," X: ",x)
+                        if(block.center.x<=1){
+                            if(isNaN(grid[block.center.y+(y-1)][block.center.x-x])||grid[block.center.y+(y-1)][block.center.x-x]==undefined){
+                                break;
+                            }
+                        }
+                        else{
+                            block.center.x--;
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+            break;
+        case "s":
+        case "ArrowDown":
+            speed = 70;
+            break;
+        case "d":
+        case "ArrowRight":
+            for(y = 0;y<block.grid.length;y++){
+                for(x=0;x<block.grid[y].length;x++){
+                    if(block.grid[y][x]==1){
+                        if(isNaN(grid[block.center.y+(y-1)][block.center.x+(x)])||grid[block.center.y+(y-1)][block.center.x+(x)]==undefined){
+                            break;
+                        }
+                        else{
+                            block.center.x++;
+                            break;
+                        }
+                    }
+                }
+                break;
+            }
+            break;
+        case " ":
+            while(!block.locked){
+                block.locked = (block.center.y==19||isNaN(grid[block.center.y+1][block.center.x]));
+                block.center.y+= !(block.center.y==19||isNaN(grid[block.center.y+1][block.center.x]));
+            }
+            break;
+    }
+};  
+
+var handleKeyUp = function (event){
+    keyValue = event.key;
+    switch(keyValue){
+        case "s":
+        case "ArrowDown":
+            speed = 140;
+            break;
+    }
+};  
+
+
+window.addEventListener('keydown', handleKeyDown, false);
+window.addEventListener('keyup', handleKeyUp, false);
 
 block = new Block;
 
+function lockBlocks(){
+    for(y = 0;y<block.grid.length;y++){
+        for(x=0;x<block.grid[y].length;x++){
+            if(block.grid[y][x]==1){
+                grid[block.center.y+(y-1)][block.center.x+(x-1)]= block.type;  
+                if(block.center.y<19){
+                    if(isNaN(grid[block.center.y+(y)][block.center.x+(x-1)])){
+                        block.locked=true;
+                    }
+                }
+                else if(block.center.y==19){
+                    block.locked=true;
+                }              
+            }
+        }
+    }
+
+}
 
 async function startGame(){
-    while(block.center[1]<20){
-        grid[block.center[1]][block.center[0]] = "I";
+    while(block.center.y<20){
         draw();
-        grid[block.center[1]][block.center[0]] = 0;
-        block.center[1] += 1;
-        await sleep(300);
+        lockBlocks();
+        block.moveDown();
+        await sleep(speed);
     }
+    block = new Block;
+    startGame();
+}
+
+function resetGame(){
+    alert("poopy")
 }
 
 startGame();
